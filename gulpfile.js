5201a831be93ed4ffe0921afbe3685457de78cff
He4eT <He4eTHb1u@gmail.com>
2014-11-09 15:11:06 +0500

    Gulp file added

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+// npm install gulp-less gulp-browserify gulp-rename gulp-autoprefixer --save-dev 
+
+var publicPath = './public/';
+var staticPath = './static/';
+
+
+// Include gulp
+var gulp = require('gulp'); 
+
+process.env.production = false;
+
+// Include Our Plugins
+var less = require('gulp-less');
+var uglify = require('gulp-uglifyjs');
+var browserify = require('gulp-browserify');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+// Compile Our less
+gulp.task('styleProcessing', function() {
+    gulp.src(staticPath + 'less/*.less')
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(staticPath + 'css'));
+});
+
+// Browserify task
+gulp.task('browserify', function() {
+    gulp.src(staticPath + 'js/app.js')
+    .pipe(browserify({insertGlobals : true, debug : !process.env.production}))
+    .pipe(uglify())
+    .pipe(gulp.dest(staticPath + 'buildjs'))
+});
+
+// Move
+gulp.task('move',['styleProcessing', 'browserify'], function(){
+  gulp.src(staticPath + 'buildjs/*.js')
+  .pipe(gulp.dest(publicPath + 'js'));
+
+  gulp.src(staticPath + 'css/*.css')
+  .pipe(gulp.dest(publicPath + 'css'));
+});
+
+// Default Task
+gulp.task('default', ['styleProcessing', 'browserify', 'move']);
