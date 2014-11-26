1da1a9918b339270e3801d3475694c88666a88d9
Kirill <krisstal.mail@gmail.com>
2014-11-26 20:44:01 +0500

    full day directive was implemented

--- static/modules/m_forecast-full-data/forecast-full-data.js
+++ static/modules/m_forecast-full-data/forecast-full-data.js
@@ -1,9 +1,18 @@
 /**
  * m_forecast-full-data
  */
-app.directive('forecastfulldata', function() {
-  return {
-    templateUrl: 'm_forecast-full-data/forecast-full-data.html',
-    restrict: 'E'
-  }
-});
+app
+    .directive('forecastfulldata', function () {
+        return {
+            templateUrl: 'm_forecast-full-data/forecast-full-data.html',
+            restrict: 'E'
+        }
+    })
+    .directive('forecastdayfull', function () {
+        return {
+            templateUrl: 'm_forecast-full-data/forecast-day-full.html',
+            restrict: 'E',
+            scope: { day: '=' , locality: '='},
+            replace: true
+        }
+    });
